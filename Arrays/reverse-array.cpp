35732634dbe08efb5dd597a3bd981cde2a3f0ad2
Khan-Ramsha <khanramsha302020@gmail.com>
2024-06-14 13:55:02 +0530

    reverse-array

---
+++ Arrays/reverse-array.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+void reversearray(int a[],int size){
+    int arr[size];
+    for(int i=0;i<size;i++){
+
+         arr[i] = a[size-i-1];
+    }
+    for (int i = 0; i < size; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+}
+int main(){
+
+    int a[] = {1, 2, -3, 4, -5, 7};
+    int n = sizeof(a) / sizeof(a[0]);
+    reversearray(a,n);
+}
+
+// copying in reverse order
