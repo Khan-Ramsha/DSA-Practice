09414718bc9f19ed20e20a243b46b37217956372
Khan-Ramsha <khanramsha302020@gmail.com>
2024-06-21 12:28:44 +0530

    Min-In-Rotated-Sorted

---
+++ Arrays/min_rotated_sorted.cpp
@@ -0,0 +1,21 @@
+
+// class Solution {
+// public:
+//     int findMin(vector<int>& nums) {
+//         int ans=INT_MAX;
+//         int low=0,high=nums.size()-1;
+//         while(low<=high){
+//             int mid=(low+high)/2;
+//             if(nums[low]<=nums[mid]){
+//                 ans=min(ans,nums[low]);
+//                 low=mid+1;
+//             }
+//             //sorted present on left
+//             else{
+//                 ans=min(ans,nums[mid]);
+//                 high=mid-1;
+//             }
+//         }
+//         return ans;
+//     }
+// };
